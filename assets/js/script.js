6da6598ece2b0aee934b0888b54bf1f45dcfa63c
ABGEO <takalandzet@gmail.com>
2019-08-17 15:27:24 +0400

    Add new message sending feature.

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,65 @@ let conversation = $('.conversation');
 $('button.clear-chat').click(function () {
     conversation.html(null);
 });
+
+/**
+ * Send message from person 1.
+ */
+$('button.send-message-1').click(function () {
+    sendNewMessage(1);
+});
+
+/**
+ * Send message from person 2.
+ */
+$('button.send-message-2').click(function () {
+    sendNewMessage(2);
+});
+
+/**
+ * Send new message.
+ *
+ * @param from Sender person (1/2).
+ */
+function sendNewMessage(from) {
+    let newMessage = $('textarea.new-message');
+    let newMessageText = newMessage.val();
+
+    if (newMessageText === '' || newMessageText === null) {
+        return;
+    }
+
+    let messagesClass;
+    if (from === 1) {
+        messagesClass = 'messages-received';
+    } else if (from === 2) {
+        messagesClass = 'messages-sent';
+    }
+
+    // Create new message wrapper.
+    let messageWrapper = $(' <div class="message"></div>');
+
+    // Add message text to message wrapper.
+    messageWrapper.text(newMessageText);
+
+    // Get last conversation box.
+    let lastConversation = conversation.children().last();
+
+    if (lastConversation.hasClass(messagesClass)) {
+        // Append to existing messages wrapper.
+        lastConversation.append(messageWrapper);
+    } else {
+        // Create new messages wrapper.
+        let messagesWrapper = $(' <div class="messages"></div>');
+        // Add class.
+        messagesWrapper.addClass(messagesClass);
+        // Append message.
+        messagesWrapper.append(messageWrapper);
+
+        // Add new message to conversation.
+        conversation.append(messagesWrapper);
+    }
+
+    // Make message input empty.
+    newMessage.val(null);
+}
